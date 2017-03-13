f6ad7c3ec82345fb3cadea743181e7af3a8c44fb
Rahul Verma <rahul.verma@capitalone.com>
2017-03-13 01:41:11 -0700

    more work in progress

---
+++ src/components/our-story-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'our-story',
+  templateUrl: './our-story-component.html'
+})
+export class OurStoryComponent {}
+
--- src/components/venue-events.component.ts
+++ src/components/venue-events.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'venue-events',
-  templateUrl: './venue-events.component.html'
+  templateUrl: './venue-events.component.html',
+  styles: [`
+    .sebm-google-map-container {
+       height: 300px;
+     }
+  `]
 })
 export class VenueEventsComponent {
-
+  title: string = 'Venue - ISKON Juhu - Sri Sri Radha Rasabihari Temple, Hare Krishna Land, Juhu Church Road, Juhu, Mumbai, Maharashtra 400049, India';
+  lat: number = 19.113135;
+  lng: number = 72.826725;
+  zoom: number = 16;
 }
