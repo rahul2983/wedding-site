c990b87cfab031832fbbe522a09551db8de43590
Rahul Verma <rahul.verma@capitalone.com>
2017-03-31 14:54:24 -0700

    trying cloudfront for serving video and adding favicon

--- src/components/intro_video.component.ts
+++ src/components/intro_video.component.ts
@@ -4,4 +4,17 @@ import { Component } from '@angular/core';
   selector: 'intro-video',
   templateUrl: './intro-video.component.html'
 })
-export class IntroVideoComponent {}
+export class IntroVideoComponent {
+  private loc: string;
+  private videoSource: string;
+
+  constructor () {
+    this.loc = window.location.hostname;
+    if (this.loc === "www.aprajitawedsrahul.com") {
+      this.videoSource = "d2j1irgaha4o6d.cloudfront.net";
+    }
+    else if (this.loc === "localhost") {
+      this.videoSource = "src/video/AKfZP3Xm8LsSjfNYIpxB.mp4";
+    }
+  }
+}
