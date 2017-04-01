86a958c5902c3cbecb63a506e108975188e96ff7
Rahul Verma <rahul.verma@capitalone.com>
2017-04-01 10:33:24 -0700

    adding more info on Contact Us and refactoring Venue and Events page

--- src/components/venue-events.component.ts
+++ src/components/venue-events.component.ts
@@ -10,8 +10,34 @@ import { Component } from '@angular/core';
   `]
 })
 export class VenueEventsComponent {
-  title: string = 'Venue - ISKON Juhu - Sri Sri Radha Rasabihari Temple, Hare Krishna Land, Juhu Church Road, Juhu, Mumbai, Maharashtra 400049, India';
+  private venue: address;
+
+  constructor() {
+    this.venue = {
+      name: "ISKON Juhu",
+      addressline1: "Sri Sri Radha Rasabihari Temple",
+      addressline2: "Hare Krishna Land",
+      street: "Juhu Church Road",
+      county: "Juhu",
+      city: "Mumbai",
+      country: "India",
+      state: "Maharashtra",
+      zipCode: "400049"
+    }
+  }
+  //For Google Maps
   lat: number = 19.113135;
   lng: number = 72.826725;
   zoom: number = 16;
+}
+interface address {
+  name: string;
+  addressline1: string;
+  addressline2: string;
+  street: string;
+  county: string;
+  city: string;
+  zipCode: string;
+  state: string;
+  country: string;
 }
